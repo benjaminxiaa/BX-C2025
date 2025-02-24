fa00d3cb0f5ca6c9a9bbf948ec4235fdeafb7750
Benjamin Xia <xiaa.benjamin@gmail.com>
2025-02-24 08:56:12 -0800

    fixed autons, finished all controller bindings

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -39,7 +39,6 @@ public class Robot extends TimedRobot {
 
       SmartDashboard.putData("LL1Field", limelightSim.getField2d());
       SmartDashboard.putData("LL2Field", limelight2Sim.getField2d());
-      SignalLogger.setPath("logs/");
     }
   }
 
@@ -47,18 +46,6 @@ public class Robot extends TimedRobot {
   public void robotPeriodic() {
     CommandScheduler.getInstance().run();
 
-    /*
-     * This example of adding Limelight is very simple and may not be sufficient for
-     * on-field use.
-     * Users typically need to provide a standard deviation that scales with the
-     * distance to target
-     * and changes with number of tags available.
-     *
-     * This example is sufficient to show that vision integration is possible,
-     * though exact implementation
-     * of how to use vision should be tuned per-robot and to the team's
-     * specification.
-     */
     if (kUseLimelight) {
       m_robotContainer.updateVision();
     }
@@ -79,6 +66,11 @@ public class Robot extends TimedRobot {
 
   @Override
   public void autonomousInit() {
+    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.schedule();
+    }
   }
 
   @Override
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,14 +4,16 @@
 
 package frc.robot;
 
-import static edu.wpi.first.units.Units.*;
+import static edu.wpi.first.units.Units.MetersPerSecond;
+import static edu.wpi.first.units.Units.RadiansPerSecond;
+import static edu.wpi.first.units.Units.RotationsPerSecond;
 
 import com.ctre.phoenix6.SignalLogger;
 import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
 import com.ctre.phoenix6.swerve.SwerveRequest;
 import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.auto.NamedCommands;
 
-import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.DataLogManager;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -19,7 +21,13 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
-import frc.robot.commands.drivetrain.AutoAlign;
+import frc.robot.commands.EE.IntakeAlgae;
+import frc.robot.commands.EE.IntakeCoral;
+import frc.robot.commands.EE.Score;
+import frc.robot.commands.elevator.DoOperator;
+import frc.robot.commands.elevator.SetOperatorPosition;
+import frc.robot.commands.elevator.ZeroElevator;
+import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.Elevator;
 import frc.robot.subsystems.EndEffector;
 import frc.robot.subsystems.swerve.Drivetrain;
@@ -35,9 +43,7 @@ public class RobotContainer {
     private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
             .withDeadband(MaxSpeed * 0.1).withRotationalDeadband(MaxAngularRate * 0.1) // Add a 10% deadband
             .withDriveRequestType(DriveRequestType.OpenLoopVoltage); // Use open-loop control for drive motors
-    private final SwerveRequest.SwerveDriveBrake brake = new SwerveRequest.SwerveDriveBrake();
-    private final SwerveRequest.PointWheelsAt point = new SwerveRequest.PointWheelsAt();
-    private final SwerveRequest.RobotCentric forwardStraight = new SwerveRequest.RobotCentric()
+    private final SwerveRequest.RobotCentric robotCentric = new SwerveRequest.RobotCentric()
             .withDriveRequestType(DriveRequestType.OpenLoopVoltage);
 
     private final Telemetry logger = new Telemetry(MaxSpeed);
@@ -51,13 +57,21 @@ public class RobotContainer {
     private final Elevator elevator = Elevator.getInstance();
     private final EndEffector endEffector = EndEffector.getInstance();
 
-    /* Path follower */
     private final SendableChooser<Command> autoChooser;
 
     public RobotContainer() {
+        NamedCommands.registerCommand("ZeroElevator", new ZeroElevator());
+        NamedCommands.registerCommand("IntakeCoral", new IntakeCoral());
+        NamedCommands.registerCommand("ElevatorHeight1",
+                new SetOperatorPosition(Constants.Elevator.LEVEL_HEIGHTS[1]).andThen(new DoOperator()));
+        NamedCommands.registerCommand("ElevatorHeight2",
+                new SetOperatorPosition(Constants.Elevator.LEVEL_HEIGHTS[2]).andThen(new DoOperator()));
+        NamedCommands.registerCommand("ElevatorHeight3",
+                new SetOperatorPosition(Constants.Elevator.LEVEL_HEIGHTS[3]).andThen(new DoOperator()));
+        NamedCommands.registerCommand("Score", new Score());
+
         autoChooser = AutoBuilder.buildAutoChooser("auton1");
-        SmartDashboard.putData("Auto Mode", autoChooser);
-        // SignalLogger.setPath("/media/sda1/");
+        SmartDashboard.putData("Auton Chooser", autoChooser);
 
         visionProcessor = new VisionProcessor(
                 drivetrain,
@@ -65,7 +79,6 @@ public class RobotContainer {
                 Constants.Vision.LL3_CONFIG);
 
         SignalLogger.start();
-        DataLogManager.start();
 
         configureBindings();
     }
@@ -104,15 +117,27 @@ public class RobotContainer {
                         },
                         elevator));
 
-        driver.rightBumper().whileTrue(new AutoAlign(drivetrain));
+        // reset the field-centric heading on button b press
+        driver.b().onTrue(drivetrain.runOnce(() -> drivetrain.seedFieldCentric()));
 
-        driver.a().whileTrue(drivetrain.applyRequest(() -> brake));
-        driver.b().whileTrue(drivetrain.applyRequest(
-                () -> point.withModuleDirection(new Rotation2d(-driver.getLeftY(), -driver.getLeftX()))));
+        driver.rightBumper().onTrue(new Score()
+                .andThen(new SetOperatorPosition(0)
+                        .andThen(new DoOperator())
+                        .andThen(new ZeroElevator())));
 
-        driver.pov(0).whileTrue(drivetrain.applyRequest(() -> forwardStraight.withVelocityX(0.5).withVelocityY(0)));
-        driver.pov(180)
-                .whileTrue(drivetrain.applyRequest(() -> forwardStraight.withVelocityX(-0.5).withVelocityY(0)));
+        driver.x().onTrue(new Score());
+
+        driver.leftBumper().onTrue(new DoOperator().andThen(new IntakeAlgae()));
+
+        driver.y().whileTrue(new ZeroElevator());
+
+        operator.x().onTrue(new SetOperatorPosition(0).andThen(new ZeroElevator()));
+        operator.y().onTrue(new SetOperatorPosition(Constants.Elevator.LEVEL_HEIGHTS[3]).andThen(new DoOperator()));
+        operator.b().onTrue(new SetOperatorPosition(Constants.Elevator.LEVEL_HEIGHTS[2]).andThen(new DoOperator()));
+        operator.a().onTrue(new SetOperatorPosition(Constants.Elevator.LEVEL_HEIGHTS[1]).andThen(new DoOperator()));
+
+        operator.leftBumper().onTrue(new SetOperatorPosition(Constants.Elevator.ALGAE_HEIGHTS[0]));
+        operator.rightBumper().onTrue(new SetOperatorPosition(Constants.Elevator.ALGAE_HEIGHTS[1]));
 
         // Run SysId routines when holding back/start and X/Y.
         // Note that each routine should be run exactly once in a single log.
@@ -122,7 +147,7 @@ public class RobotContainer {
         driver.start().and(driver.x()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kReverse));
 
         // reset the field-centric heading on left bumper press
-        driver.leftBumper().onTrue(drivetrain.runOnce(() -> drivetrain.seedFieldCentric()));
+        driver.b().onTrue(drivetrain.runOnce(() -> drivetrain.seedFieldCentric()));
 
         drivetrain.registerTelemetry(logger::telemeterizeDrive);
         logger.telemeterize(elevator, endEffector);
@@ -132,8 +157,7 @@ public class RobotContainer {
         visionProcessor.update();
     }
 
-    // public Command getAutonomousCommand() {
-    /* Run the path selected from the auto chooser */
-    // return autoChooser.getSelected();
-    // }
+    public Command getAutonomousCommand() {
+        return autoChooser.getSelected();
+    }
 }
